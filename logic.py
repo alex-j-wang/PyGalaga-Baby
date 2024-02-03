d790de37dce229fe232fe5fad7327cfc51dbf636
Alex Wang <alexander_wang@brown.edu>
2024-02-03 14:02:52 -0500

    Added level parsing

--- logic.py
+++ logic.py
@@ -3,6 +3,26 @@
 # multiplayer?
 # more fighter abilities
 
+# TODO
+# update regex with correct letter range
+
+import re
+
+parse_normal = re.compile('([A-Z]) (\d+) (\d+)')
+parse_range = re.compile('([A-Z]) (\d+)-(\d+) (\d+)')
+
+def parse_level(level):
+    enemies = []
+    for line in level.split('\n'):
+        if parse_normal.match(line):
+            name, x, y = parse_normal.match(line).groups()
+            enemies.append(Enemy(name, int(x), int(y)))
+        elif parse_range.match(line):
+            name, x1, x2, y = parse_range.match(line).groups()
+            for x in range(int(x1), int(x2) + 1):
+                enemies.append(Enemy(name, x, int(y)))
+    return enemies
+
 class Enemy:
     def __init__(self, name, x, y):
         self.name = name
@@ -15,8 +35,18 @@ class Player:
         self.x = 50 # may need to be updated
         self.y = 10 # may need to be updated
 
+class Game:
+    def __init__(self, start=1):
+        self.level = start
+        with open(f'levels/L{start}.txt') as f:
+            self.enemies = parse_level(f.read())
+        self.player = Player()
+
 def main():
-    player = Player()
+    game = Game()
+    print(game.level)
+    print(game.enemies)
+    print(game.player)
 
 if __name__ == "__main__":
     main()

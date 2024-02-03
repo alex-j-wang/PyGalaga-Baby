db124e0a7f8f309384feb103e3cd435faede1ae7
Alex Wang <alexander_wang@brown.edu>
2024-02-03 13:38:38 -0500

    Player and main loop

--- logic.py
+++ logic.py
@@ -1,6 +1,23 @@
+# IDEAS
+# sandbox mode
+# multiplayer?
+# more fighter abilities
+
 class Enemy:
     def __init__(self, name, health, x, y):
         self.name = name
         self.health = health
         self.x = x
         self.y = y
+
+class Player:
+    def __init__(self):
+        self.health = 3
+        self.x = 50 # may need to be updated
+        self.y = 10 # may need to be updated
+
+def main():
+    player = Player()
+
+if __name__ == "__main__":
+    main()

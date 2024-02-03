d20ed8ffc5b36103acc70648ace30d77f8ef40b0
Aaoki2023 <mr.aaoki@gmail.com>
2024-02-03 10:50:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import pygame
+
+pygame.init()
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+def main():
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT :
+				run = False
+		pygame.display.update()
+	pygame.quit()
+
+main()
+
+
+

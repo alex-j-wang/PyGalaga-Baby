5bfffdf52dd396f2f6315883bdee1594c9e7cade
Aaoki2023 <mr.aaoki@gmail.com>
2024-02-03 16:13:06 -0800

    cleanup

--- main.py
+++ main.py
@@ -27,48 +27,57 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock();
-	fps = 40
-	game = Game()
 	
 	pygame.display.set_caption("Galaga")
 
 	bg = pygame.image.load("background.png").convert()
 	bg_height = bg.get_height()
 	tiles = math.ceil(SCREEN_HEIGHT / bg_height) + 2
-	
-	scroll = 0
-
-
-	bullets = []  # List to store bullet rectangles
-	last_bullet_time = 0
 
 	player_image = pygame.image.load("rocket-removebg-preview.png").convert_alpha()
 	player = Player(player_image)
 	player.rect.x = 185
 	player.rect.y = 500
-	can_fire = True
-	run = True
-	while run:
-		clock.tick(fps)
-		for i in range(tiles):
-			screen.blit(bg, (0, SCREEN_HEIGHT - (i * bg_height) - scroll))
-		scroll -= 5
-		if abs(scroll) > bg_height:
-			scroll = 0
+	
+	loop(clock, screen, player, bg, tiles)
 
-		game.display_enemies(screen)
-
-		for bullet in bullets:
+def shooting(bullets, game, screen):
+	for bullet in bullets:
 			pygame.draw.rect(screen, (0, 255, 0), bullet)  # Draw bullets as green rectangles
 
 
 	# Check collision between bullets and enemies
-		for bullet in bullets:
+	for bullet in bullets:
 			for enemy in game.enemies:  # Same here
 				if collides(bullet, pygame.Rect(enemy.x, enemy.y, 25, 25)):
 					bullets.remove(bullet)
 					game.enemies.remove(enemy)
 					break
+<<<<<<< Updated upstream
+=======
+
+		# Update bullet positions
+	for bullet in bullets:
+			bullet.move_ip(0, -10)  # Adjust the bullet speed as needed
+		# Remove bullets that have gone off-screen
+	bullets = [bullet for bullet in bullets if bullet.y > 0]
+
+
+def loop(clock, screen, player, bg, tiles):
+	game, scroll, run, can_fire, fps, last_bullet_time = Game(), 0, True, True, 40, 0
+	bullets = []  # List to store bullet rectangles
+	while run:
+		clock.tick(fps)
+		for i in range(tiles):
+			screen.blit(bg, (0, SCREEN_HEIGHT - (i * bg.get_height()) - scroll))
+		scroll -= 5
+		if abs(scroll) > bg.get_height():
+			scroll = 0
+
+		game.display_enemies(screen)
+
+		shooting(bullets, game, screen)
+>>>>>>> Stashed changes
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -78,6 +87,10 @@ def main():
 
 		key = pygame.key.get_pressed()
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 		time_since_last_bullet = pygame.time.get_ticks() - last_bullet_time
 		if (time_since_last_bullet >= BULLET_COOLDOWN):
 			can_fire = True
@@ -85,12 +98,12 @@ def main():
 			can_fire = False
 
 		last_bullet_time = key_event(key, player, bullets, last_bullet_time, can_fire)
+<<<<<<< Updated upstream
+=======
+		
+>>>>>>> Stashed changes
 
-		# Update bullet positions
-		for bullet in bullets:
-			bullet.move_ip(0, -10)  # Adjust the bullet speed as needed
-		# Remove bullets that have gone off-screen
-		bullets = [bullet for bullet in bullets if bullet.y > 0]
+		
 
 
 		#key_event(key, player)
